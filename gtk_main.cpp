ccc2dbcfac41c43437b05ae74030e44a3dd0fce9
zxie <thelearner29042004@gmail.com>
2024-12-22 11:23:13 +0530

    adding cmake

--- gtk_main.cpp
+++ gtk_main.cpp
@@ -1,13 +1,17 @@
-
 #include <gtkmm.h>
 
+class MyWindow : public Gtk::Window {
+public:
+  MyWindow();
+};
+
+MyWindow::MyWindow() {
+  set_title("Basic application");
+  set_default_size(200, 200);
+}
 
 int main(int argc, char *argv[]) {
-    auto app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
+  auto app = Gtk::Application::create("org.gtkmm.examples.base");
 
-    Gtk::Window window;
-    window.set_default_size(400, 200);
-    window.set_title("Simple GTK Window");
-
-    return app->run(window);
+  return app->make_window_and_run<MyWindow>(argc, argv);
 }
