9aadf283899259144875dc92c147430b8009ff4d
zxie <thelearner29042004@gmail.com>
2025-01-31 20:47:38 +0530

    work in progress to figure out 'outofbound' error when massing configV in organisefolder function

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,26 @@
 #include "../include/config.h"
+#include "../include/core.h"
+#include <iostream>
 
+// global variables
 int main() {
+  ConfigParser config;
+  fileOperations operate;
   const std::string filename =
       "/home/himanshu/code/gtk_multimedia_organiser/include/config.conf";
-  ConfigParser parsed;
-  parsed.parseConfigFile(filename);
 
-  parsed.printConfig();
-  return 0;
+  const configV configV_umap = config.parseConfigFile(filename);
+
+  std::vector<std::string> vec =
+      configV_umap.at("source_directory_1").at("source_path");
+
+  std::cout << vec.size() << "\n";
 }
+
+// std::vector<std::string> vec =
+//     configV_umap.at("source_directory_1").at("source_path");
+
+// std::cout << vec.size() << "\n";
+
+// config.printConfig(configV_umap);
+// operate.organise_folder(configV_umap);
