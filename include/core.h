a66da92d955375b2f3f74bbfa713059dd4201a89
zxie <thelearner29042004@gmail.com>
2025-01-31 20:44:16 +0530

    organisefolder function implemented that takes up the input from parsed configfile

--- include/core.h
+++ include/core.h
@@ -1,20 +1,16 @@
+#pragma once
+#include "global.h"
 #include <cstdlib> // For getenv
-#include <string>  // For std::string
+#include <filesystem>
+#include <string> // For std::string
 
-// people who are using gcc built versions older tan 8.1.0 will have to use the
-// following code replace the #include <filesystem> with the following code
-
-// TODO: Hardcoding Downloads directory using platform-specific Env Variables
-
-#ifdef _WIN32
-const std::string SOURCE =
-    std::string(std::getenv("USERPROFILE")) + "\\Desktop\\test";
-#elif defined(__linux__)
-const std::string SOURCE = std::string(std::getenv("HOME")) + "/Downloads";
-#else
-#error Unsupported Operating System
-#endif
-
-std::string getfile_ext(const std::string &filename);
-// std::vector<std::string> getfiles_fromDir(const std::string &SOURCE);
-void movefile_indir(const std::string &SOURCE);
+class fileOperations {
+public:
+  std::string getfile_ext(const std::filesystem::path &filename);
+  std::filesystem::path createFolder(const std::string &DEST_PATH,
+                                     const std::filesystem::path &folder);
+  void defaultMoveBehaviour(const std::string &SOURCE);
+  void moveFile_inDir(const std::string &SOURCE, const std::string &DEST_PATH,
+                      const key_value &key_umap);
+  void organise_folder(const configV &configV_umap);
+};
--- src/core.cpp
+++ src/core.cpp
@@ -2,12 +2,7 @@
 #include <filesystem>
 #include <iostream>
 
-// people who are using gcc built versions older tan 8.1.0 will have to use the
-// following code replace the #include <filesystem> with the following code
-// #include <experimental/filesystem>  and replace std::filesystem with
-// std::experimental::filesystem or update your gcc version
-
-std::string getfile_ext(const std::filesystem::path &filename) {
+std::string fileOperations::getfile_ext(const std::filesystem::path &filename) {
   std::string ext = filename.extension().string();
   if (!ext.empty() && ext.front() == '.') {
     return ext.substr(1);
@@ -17,38 +12,103 @@ std::string getfile_ext(const std::filesystem::path &filename) {
                // that is not needed
 }
 
-void movefile_indir(const std::string &SOURCE) {
+std::filesystem::path
+fileOperations::createFolder(const std::string &DEST_PATH,
+                             const std::filesystem::path &folder) {
 
-  // NOTE: Get files from the specified dir this can be consolidated into a
-  // function but then we have to read it aswell there wasnt muchto gain so i
-  // kept it in the same function
+  std::filesystem::path dest_path(DEST_PATH);
+  if (!std::filesystem::exists(DEST_PATH / folder)) {
+    std::filesystem::create_directory(DEST_PATH / folder);
+  }
+  dest_path = dest_path / folder;
+  return dest_path;
+}
 
+void fileOperations::defaultMoveBehaviour(const std::string &SOURCE) {
+
+  std::filesystem::path SourceDir(SOURCE);
   for (const auto &file : std::filesystem::directory_iterator(SOURCE)) {
-    std::filesystem::path SourceDir(SOURCE);
-    if (std::filesystem::is_regular_file(file.status())) {
+    if (std::filesystem::is_regular_file(file)) {
       std::string ext = getfile_ext(file);
       std::filesystem::path file_path(file);
       std::filesystem::path folder(ext + "_folder");
 
-      if (!std::filesystem::exists(SourceDir / folder)) {
-        std::filesystem::create_directory(SourceDir / folder);
-      }
-
-      std::filesystem::path dest_path =
-          SourceDir / folder / file_path.filename();
-
-      if (std::filesystem::exists(file)) {
+      // if (!std::filesystem::exists(SourceDir / folder)) {
+      //   std::filesystem::create_directory(SourceDir / folder);
+      // }
+      //
+      // std::filesystem::path dest_path =
+      //     SourceDir / folder / file_path.filename();
+      std::filesystem::path dest_path = createFolder(SourceDir, folder);
+      if (std::filesystem::exists(file_path)) {
         try {
-          std::filesystem::rename(file_path, dest_path);
+          std::filesystem::rename(file_path, dest_path / file_path.filename());
 
-          std::cout << "Moved: " << file_path.string() << " -> "
-                    << dest_path.string() << std::endl;
         } catch (const std::exception &e) {
           std::cerr << "Error moving file: " << file_path << " - " << e.what()
                     << std::endl;
         }
+      }
+    }
+  }
+}
+
+void fileOperations::moveFile_inDir(const std::string &SOURCE,
+                                    const std::string &DEST_PATH,
+                                    const key_value &key_umap) {
+  std::filesystem::path dest_path;
+  std::filesystem::path SourceDir(SOURCE);
+  for (const auto &pair : key_umap) {
+    if (pair.first != "source_path" || pair.first != "dest_path") {
+      std::filesystem::path folder(pair.first);
+      for (const auto &extension : pair.second) {
+        for (const auto &file : std::filesystem::directory_iterator(SOURCE)) {
+          if (std::filesystem::is_regular_file(file)) {
+            std::filesystem::path file_path(file);
+            if (file_path.extension() == extension) {
+              dest_path = createFolder(DEST_PATH, folder);
+              if (std::filesystem::exists(file)) {
+                try {
+                  std::filesystem::rename(file_path,
+                                          dest_path / file_path.filename());
+
+                } catch (const std::exception &e) {
+                  std::cerr << "Error moving file: " << file_path << " - "
+                            << e.what() << dest_path << std::endl;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+void fileOperations::organise_folder(const configV &configV_umap) {
+  // key_value key_umap;
+  std::string SOURCE;
+  std::string DEST_PATH;
+  if (!configV_umap.empty()) {
+    for (const auto &section : configV_umap) {
+      if (section.first == "settings") {
+
+        // TODO:implement settings
+
       } else {
-        std::cerr << "File not found: " << file_path.string() << std::endl;
+        if (configV_umap.count(section.first) &&
+            configV_umap.at(section.first).count("source_path")) {
+          SOURCE = configV_umap.at(section.first).at("source_path")[0];
+          std::cout << SOURCE << std::endl;
+          if (configV_umap.count(section.first) &&
+              configV_umap.at(section.first).count("dest_path")) {
+            DEST_PATH = configV_umap.at(section.first).at("dest_path")[0];
+          } else {
+            DEST_PATH = SOURCE;
+          }
+        }
+        moveFile_inDir(SOURCE, DEST_PATH, section.second);
+        // move file based on the key value map which is section.second here
       }
     }
   }
