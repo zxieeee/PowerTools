d589193c6be1716f553207e26dd1ae55ba8af877
aryan mishra <83206527+User719-blip@users.noreply.github.com>
2025-02-08 11:12:42 +0000

    trim spaces implementation

--- src/main.cpp
+++ src/main.cpp
@@ -7,20 +7,14 @@ int main() {
   ConfigParser config;
   fileOperations operate;
   const std::string filename =
-      "/home/himanshu/code/gtk_multimedia_organiser/include/config.conf";
+      "../include/config.conf";
 
   const configV configV_umap = config.parseConfigFile(filename);
 
-  std::vector<std::string> vec =
-      configV_umap.at("source_directory_1").at("source_path");
+  config.printConfig(configV_umap);
+  operate.organise_folder(configV_umap);
 
-  std::cout << vec.size() << "\n";
 }
 
-// std::vector<std::string> vec =
-//     configV_umap.at("source_directory_1").at("source_path");
 
-// std::cout << vec.size() << "\n";
 
-// config.printConfig(configV_umap);
-// operate.organise_folder(configV_umap);
